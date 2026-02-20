6ff125105fb7cde6761a38bc19b3ff739da90d42
samthestrategist <sam@littlemissmineral.com>
2026-02-19 22:47:59 -0800

    Update CRC images: branded labels

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,12 +4,12 @@ import { Helmet } from "react-helmet-async";
 const PDP_URL = "https://samthestrategist.github.io/lmm-centella-restore-cream-pdp/";
 
 const CDN = {
-  heroFront: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/CDN_HERO_FRONT_69629f94-9191-46b0-91cc-94fee07c07b5.png?v=1771555411",
-  heroAngle: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/CDN_HERO_ANGLE_c1669b29-ab9f-451d-9457-b58cdee630fc.png?v=1771555414",
-  lifestyleHands: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/CDN_LIFESTYLE_1_5a7d6696-1bc8-4945-aa53-52b9d8ea7cf2.png?v=1771555419",
-  textureSwatch: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/CDN_LIFESTYLE_1_5a7d6696-1bc8-4945-aa53-52b9d8ea7cf2.png?v=1771555419",
-  bundle3jars: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/CDN_BUNDLE_3_57522eac-56c8-413e-a29b-b7ee8e82e4f0.png?v=1771555417",
-  adOverlay: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/CDN_AD_OVERLAY_47beecd5-cf45-4379-b421-d9532013cae3.png?v=1771555422",
+  heroFront: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/crc-hero-front_26ad5e04-3c33-4c93-afd1-dff74707da43.png?v=1771569957",
+  heroAngle: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/crc-hero-angle_e2e548fa-1756-4701-8f69-83924cbc2201.png?v=1771569970",
+  lifestyleHands: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/crc-texture-swatch_33b5eff6-8a2c-40fb-998a-df3c3a9cab8c.png?v=1771569979",
+  textureSwatch: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/crc-texture-swatch_33b5eff6-8a2c-40fb-998a-df3c3a9cab8c.png?v=1771569979",
+  bundle3jars: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/crc-bundle-3jars_10d120e7-f7d8-41fd-85d6-fa5fc6ce1d32.png?v=1771569973",
+  adOverlay: "https://cdn.shopify.com/s/files/1/0462/8591/4267/files/crc-ad-overlay_316b6422-0fa4-438f-ac87-17ad74a35734.png?v=1771569982",
 };
 
 const COLORS = {
